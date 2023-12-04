599d78312f488d78996aa83665f92324e63af839
Barber <barber@MT-306070.jpl.nasa.gov>
2023-12-04 17:41:50 +0000

    automate a bit more

--- terraform-unity/terraform.tf
+++ terraform-unity/terraform.tf
@@ -41,12 +41,36 @@ resource "aws_ecs_cluster" "httpd_cluster" {
 resource "aws_efs_file_system" "httpd_config_efs" {
   creation_token = "httpd-config"
 }
+resource "aws_security_group" "efs_sg" {
+  name        = "efs-security-group"
+  description = "Security group for EFS"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
+  # Ingress rule to allow NFS
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.ecs_sg.id]
+  }
+
+  # Egress rule - allowing all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "efs-security-group"
+  }
+}
 resource "aws_efs_mount_target" "efs_mount_target" {
   for_each          = toset(local.subnet_ids)
   file_system_id     = aws_efs_file_system.httpd_config_efs.id
   subnet_id         = each.value
-  security_groups    = ["sg-051a5abe923b5a595"]
+  security_groups    = [aws_security_group.efs_sg]
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
