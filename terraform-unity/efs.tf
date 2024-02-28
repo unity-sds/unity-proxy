fc7f1ed507b4e8dd80193e05397a3e6792174fba
barber <tom@spicule.co.uk>
2024-02-28 18:57:37 +0000

    update mounts

--- terraform-unity/efs.tf
+++ terraform-unity/efs.tf
@@ -51,7 +51,7 @@ resource "aws_efs_access_point" "httpd_config_ap" {
     creation_info {
       owner_gid   = 1000
       owner_uid   = 1000
-      permissions = "0755"
+      permissions = "0777"
     }
   }
 
