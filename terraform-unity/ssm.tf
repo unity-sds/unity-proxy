8cafef383e11362545d9e87013c514d232877593
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2024-05-29 16:56:52 -0700

    updating ssm entery per testing

--- terraform-unity/ssm.tf
+++ terraform-unity/ssm.tf
@@ -20,15 +20,17 @@ resource "aws_ssm_parameter" "managementproxy_config" {
   name       = "/unity/${var.project}/${var.venue}/cs/management/proxy/configurations/010-management"
   type       = "String"
   value      = <<-EOT
+
+    RewriteEngine on
+    RewriteCond %{HTTP:Upgrade} websocket [NC]
+    RewriteCond %{HTTP:Connection} upgrade [NC]
+    RewriteRule /management/(.*) ws://${var.mgmt_dns}/$1 [P,L]
     <Location "/management/">
-        RewriteEngine on
         ProxyPass http://${var.mgmt_dns}/
         ProxyPassReverse http://${var.mgmt_dns}/
         ProxyPreserveHost On
-        RewriteCond %\{HTTP:Upgrade} websocket [NC]
-        RewriteCond %\{HTTP:Connection} upgrade [NC]
-        RewriteRule (.*) ws://${var.mgmt_dns}/$1 [P,L]
         FallbackResource /management/index.html
     </Location>
+
 EOT
 }
