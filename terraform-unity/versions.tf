71d19c9726e2a9b900f1bc2096f1f078f3848b85
Bradley Lunsford <bradley.t.lunsford@jpl.nasa.gov>
2024-10-11 14:42:21 -0700

    adding provider entries

---
+++ terraform-unity/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.8.2"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+}
